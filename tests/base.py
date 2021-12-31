97ce60116977775f20e98452e68b1158d5a84b0f
Demetri <developerDemetri@users.noreply.github.com>
2021-12-31 01:44:39 -0800

    Initial Repo Setup (#1)
    * boilerplate setup
    
    * add basic CI action

---
+++ tests/base.py
@@ -0,0 +1,9 @@
+from unittest.case import TestCase
+
+
+class BibliariusTest(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+    def tearDown(self) -> None:
+        super().tearDown()
